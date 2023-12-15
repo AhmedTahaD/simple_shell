fee5d2440812137009e8acfe0172cecbb5321e3d
AhmedTahaD <ahmedtaha2893@gmail.com>
2023-12-15 20:48:24 +0200

    Read command finction

---
+++ read_command.c
@@ -0,0 +1,19 @@
+#include "my_pretty_shell.h"
+/*
+ * _readcommand function to read the input from user
+ * @command : command to read
+ */
+
+void _read_command(char *command) {
+
+    fgets(command, MAX_INPUT_SIZE, stdin);
+    
+    if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
+            printf("\n");
+            break;
+        }
+    size_t length = strlen(command);
+    if (length > 0 && command[length - 1] == '\n') {
+        command[length - 1] = '\0';
+    }
+}
